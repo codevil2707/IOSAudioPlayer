351a2e49c569349c476cb41cc9eb877753fc3ade
codevil2707 <himanshu.mobisoft@gmail.com>
2022-02-12 13:28:13 +0530

    creaate New Project

---
+++ IOSAudioPlayer/IOSAudioPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSAudioPlayer
+//
+//  Created by SimplyEntertaining on 2/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
